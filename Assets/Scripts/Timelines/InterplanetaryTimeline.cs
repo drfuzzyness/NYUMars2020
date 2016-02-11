05a1ad715d52e559a289e096e5d6f722a9f480c1
Matthew Conto <mbsconto@gmail.com>
2016-02-11 16:51:14 -0500

    Imported existing models, added camera fade code

---
+++ Assets/Scripts/Timelines/InterplanetaryTimeline.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class InterplanetaryTimeline : MonoBehaviour {
+	
+	public GameObject player;
+	
+	
+	[HeaderAttribute("Times")]
+	public float openingDuration;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Opening());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	IEnumerator Opening() {
+		CameraManager.instance.StartCameraFadeTo( 0f, openingDuration );
+		yield return new WaitForSeconds( openingDuration );
+	}
+
+}
