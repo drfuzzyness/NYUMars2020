db36f2510607c71ece55be5e87b2162000fad6e3
Matthew Conto <mbsconto@gmail.com>
2016-02-25 14:26:00 -0500

    Perhaps fixed massive obj file

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (NavMeshAgent))]
+[RequireComponent (typeof (Rigidbody))]
+public class Mob : MonoBehaviour {
+
+	public bool walkOnStart;
+
+	[Header("State")]
+	private float currentSpeed;
+	public bool isWalking;
+	public bool isRunning;
+
+	[Header("Balance")]
+	public float walkSpeed;
+	public float runSpeed;
+	public float stoppedDrag;
+
+	private NavMeshAgent agent;
+	private Rigidbody rbody;
+
+	public void turnToFace( Transform target ) {
+// 		Debug.LogWarning( "turnToFace() is depricated." );
+		// clamps Z and X and looks at target transform
+		Vector3 prevRot = transform.eulerAngles;
+		transform.LookAt( target );
+		Vector3 newRot = transform.eulerAngles;
+		newRot.x = prevRot.x;
+		newRot.z = prevRot.z;
+		rbody.rotation = Quaternion.Euler( newRot );
+// 		agent.	
+	}
+	
+
+	public void setDestination( Vector3 target ) {
+		agent.SetDestination( target );
+	}
+
+	public void walk() {
+		agent.speed = runSpeed;
+		agent.Resume();
+//		isWalking = true;
+//		isRunning = false;
+//		GetComponent<Rigidbody>().drag = .01f;
+//		currentSpeed = walkSpeed;
+
+	}
+
+	public void run() {
+		agent.speed = runSpeed;
+		agent.Resume();
+//		isWalking = false;
+//		isRunning = true;
+//		GetComponent<Rigidbody>().drag = .01f;
+//		currentSpeed = runSpeed;
+	}
+
+	public void stop() {
+		agent.Stop();
+//		isWalking = false;
+//		isRunning = false;
+//		currentSpeed = 0f;
+//		GetComponent<Rigidbody>().drag = stoppedDrag;
+	}
+
+	void Start() {
+		agent = GetComponent<NavMeshAgent>();
+		rbody = GetComponent<Rigidbody>();
+//		stop();
+//		if( walkOnStart ) {
+//			walk();
+//		}
+	}
+
+	// Update is called once per frame
+//	void FixedUpdate () {
+//		// move forward if walking or running and is moving slower than maxspeed
+//		if( ( isWalking || isRunning ) && GetComponent<Rigidbody>().velocity.magnitude < currentSpeed ) {
+//			GetComponent<Rigidbody>().AddForce( transform.forward * currentSpeed / 4, ForceMode.VelocityChange );
+//		}
+//	}
+}
---
+++ Assets/Scripts/RouteWaypoint.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RouteWaypoint : MonoBehaviour {
+
+	public RouteWaypoint next;
+	public float delay = 0f;
+
+	void OnDrawGizmos() {
+		if( next != null ) {
+			Vector3 vectToNext = next.transform.position - transform.position;
+			Gizmos.DrawRay( transform.position, vectToNext);
+		}
+//		Gizmos.DrawWireSphere( transform.position, .4f );
+	}
+
+	void OnTriggerEnter( Collider col ) {
+		if( col.gameObject.GetComponent<WalkOnRoute>().isActiveAndEnabled 
+		   && col.gameObject.GetComponent<WalkOnRoute>().nextWayPoint == this ) {
+			arrived( col.gameObject.GetComponent<WalkOnRoute>() );
+		}
+	}
+
+	void arrived( WalkOnRoute walker ) {
+		StartCoroutine( ApplyDelay( walker ) );
+	}
+
+	IEnumerator ApplyDelay( WalkOnRoute walker ) {
+		if( walker.arrived() ) {
+			for( float timer = 0f; timer < delay; timer += Time.deltaTime ) {
+				if( walker.inTransit )
+					timer -= Time.deltaTime; // cheeky way to pause the countdown if the char is aggressing
+				yield return null;
+			}
+//			yield return new WaitForSeconds( delay );
+//			Debug.Log( "changing to next waypoint" );
+			walker.setWaypoint( next );
+			if( walker.keepMoving ) {
+				walker.startTransit();
+			}
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
