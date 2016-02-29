5b63c29ddae043f033d47b8ce0fd8dee8ed76bd2
Matthew Conto <mbsconto@gmail.com>
2016-02-29 00:17:54 -0500

    Working on Wireframes, Sound for classroom done

--- Assets/Scripts/Timelines/ClassroomTimeline.cs
+++ Assets/Scripts/Timelines/ClassroomTimeline.cs
@@ -17,7 +17,13 @@ public class ClassroomTimeline : MonoBehaviour {
 	public CameraShake launchpadCam;
 	public float peakLaunchpadCamShake;
 	public CameraShake rocketCam;
+	[HeaderAttribute("Movements")]
 	public SpaceRouteSegment rocketLaunchSegment;
+	public SpaceRouteSegment movePlayerToChairSegment;
+	[HeaderAttribute("Sounds")]
+	public AudioController countdownAudio;
+	public AudioController classroomAmbienceAudio;
+	public AudioController controlroomAmbienceAudio;
 
 	// Use this for initialization
 	void Start () {
@@ -32,13 +38,16 @@ public class ClassroomTimeline : MonoBehaviour {
 	IEnumerator Opening() {
 		room.SetActive( false );
 		launchpad.SetActive( false );
+		classroomAmbienceAudio.Play();
 		yield return new WaitForSeconds( classroomDelay );
+		classroomAmbienceAudio.Halt();
 		StartCoroutine( RocketLaunch() );
 		Color startColor = classroomSphere.GetComponent<MeshRenderer>().material.GetColor("_TintColor");
 		Color endColor = startColor;
 		endColor.a = 0;
 		room.SetActive( true );
 		launchpad.SetActive( true );
+		controlroomAmbienceAudio.Play();
 		for( float time = 0f; time < classroomTransition; time += Time.deltaTime ) {
 				float ratio = time / classroomTransition;
 				Color newColor = Color.Lerp( startColor, endColor, ratio );
@@ -46,17 +55,24 @@ public class ClassroomTimeline : MonoBehaviour {
 				yield return null;
 		}
 		classroomSphere.gameObject.SetActive( false );
+		movePlayerToChairSegment.StartRoute();
 	}
 	
 	IEnumerator RocketLaunch() {
+		bool startedCountdownAudio = false;
 		for( float time = 0f; time < rocketLaunchDelay; time += Time.deltaTime ) {
 			int seconds = (int) Mathf.Floor( rocketLaunchDelay - time ) % 60;
 			int milliseconds = (int) ( ( rocketLaunchDelay - time ) * 1000 ) % 1000 ;
-			uiCountdownText.text = "00:00:" + seconds + ":" + milliseconds;
+			uiCountdownText.text = "00:00:" + seconds + "." + milliseconds;
 			float ratio = time / rocketLaunchDelay;
 			launchpadCam.shakeAmount = Mathf.Lerp( 0, peakLaunchpadCamShake, ratio );
+			if( !startedCountdownAudio && time > rocketLaunchDelay - 12f ) {
+				countdownAudio.Play();
+				startedCountdownAudio = true;
+			}
 			yield return null;
 		}
+		uiCountdownText.text = "00:00:00.000";
 		// launch happened
 		rocketLaunchSegment.StartRoute();
 		yield return new WaitForSeconds( switchToRocketCamDelay );
